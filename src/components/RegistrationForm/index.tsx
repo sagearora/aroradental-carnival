dc601782c99af2070d2cee2d8c6af64dc1b0c53e
Saj Arora <saj.arora24@gmail.com>
2024-06-22 10:31:44 -0400

    finished registration form with waiver

--- src/components/RegistrationForm/index.tsx
+++ src/components/RegistrationForm/index.tsx
@@ -189,7 +189,11 @@ function RegistrationForm({
                         type='submit'
                         size='lg'
                         disabled={loading}>
-                        {loading && <Loader2 className='w-4 h-4 animate-spin' />}
+                        {loading ? <Loader2 className='size-8 animate-spin mr-2' /> : <svg xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24" fill="currentColor" className="size-8 mr-2">
+                            <path fillRule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
+                        </svg>
+                        }
                         COMPLETE REGISTRATION
                     </Button>
                     <p className="mb-4">
